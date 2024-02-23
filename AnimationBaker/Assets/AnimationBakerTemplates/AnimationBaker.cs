d1effcc931939ca5dc8216c69361c81105eaadde
ochiantropos <aggyssss.dt2@gmail.com>
2024-02-23 10:16:33 +0200

    bake textures

--- AnimationBaker/Assets/AnimationBakerTemplates/AnimationBaker.cs
+++ AnimationBaker/Assets/AnimationBakerTemplates/AnimationBaker.cs
@@ -55,109 +55,116 @@ public class AnimationBaker : MonoBehaviour
         int index = 0;
         foreach (var clip in clips)
         {
+            index = BakeClip(index, clip, texWidth, skin, mesh, vCount);
+        }
+    }
 
-            try
-            {
-                if (saverAnimationName[index]== "") saverAnimationName[index] = clip.name;
+    private int BakeClip(int index, AnimationClip clip, int texWidth, SkinnedMeshRenderer skin, Mesh mesh, int vCount)
+    {
+        try
+        {
+            if (saverAnimationName[index]== "") saverAnimationName[index] = clip.name;
  
 
-                var frames = Mathf.NextPowerOfTwo((int)(clip.length / 0.05f));
-                var dt = clip.length / frames;
-                var infoList = new List<VertInfo>();
+            var frames = Mathf.NextPowerOfTwo((int)(clip.length / 0.05f));
+            var dt = clip.length / frames;
+            var infoList = new List<VertInfo>();
 
-                var pRt = new RenderTexture(texWidth, frames, 0, RenderTextureFormat.ARGBHalf);
-                pRt.name = string.Format("{0}.{1}.posTex", saverObjectName, saverAnimationName[index]);
-                var nRt = new RenderTexture(texWidth, frames, 0, RenderTextureFormat.ARGBHalf);
-                nRt.name = string.Format("{0}.{1}.normTex", saverObjectName, saverAnimationName[index]);
-                var tRt = new RenderTexture(texWidth, frames, 0, RenderTextureFormat.ARGBHalf);
-                tRt.name = string.Format("{0}.{1}.tanTex", saverObjectName, saverAnimationName[index]);
-                foreach (var rt in new[] { pRt, nRt, tRt })
-                {
-                    rt.enableRandomWrite = true;
-                    rt.Create();
-                    RenderTexture.active = rt;
-                    GL.Clear(true, true, Color.clear);
-                }
-
-                for (var i = 0; i < frames; i++)
-                {
-                    clip.SampleAnimation(gameObject, dt * i);
-                    skin.BakeMesh(mesh);
-
-                    var verexArry = mesh.vertices;
-                    var normalArry = mesh.normals;
-                    var tangentArray = mesh.tangents;
-                    infoList.AddRange(Enumerable.Range(0, vCount)
-                        .Select(idx => new VertInfo()
-                        {
-                            position = verexArry[idx],
-                            normal = normalArry[idx],
-                            tangent = tangentArray[idx],
-                        })
-                    );
-                }
-                var buffer = new ComputeBuffer(infoList.Count, System.Runtime.InteropServices.Marshal.SizeOf(typeof(VertInfo)));
-                buffer.SetData(infoList.ToArray());
-
-                var kernel = infoTexGen.FindKernel("CSMain");
-                uint x, y, z;
-                infoTexGen.GetKernelThreadGroupSizes(kernel, out x, out y, out z);
-
-                infoTexGen.SetInt("VertCount", vCount);
-                infoTexGen.SetBuffer(kernel, "Info", buffer);
-                infoTexGen.SetTexture(kernel, "OutPosition", pRt);
-                infoTexGen.SetTexture(kernel, "OutNormal", nRt);
-                infoTexGen.SetTexture(kernel, "OutTangent", tRt);
-                infoTexGen.Dispatch(kernel, vCount / (int)x + 1, frames / (int)y + 1, 1);
-
-                buffer.Release();
-
-    #if UNITY_EDITOR
-                var folderPath = Path.Combine("Assets", saverFolderName);
-                if (!AssetDatabase.IsValidFolder(folderPath))
-                    AssetDatabase.CreateFolder("Assets", saverFolderName);
-                var dFolderPath = Path.Combine(folderPath, saverObjectName + "_Animation"); 
-                if (!AssetDatabase.IsValidFolder(dFolderPath))
-                    AssetDatabase.CreateFolder(folderPath, saverObjectName + "_Animation");
-                var subFolderPath =Path.Combine(dFolderPath, saverObjectName + "_Texture_Assets");
-                if (!AssetDatabase.IsValidFolder(subFolderPath))
-                    AssetDatabase.CreateFolder(dFolderPath, saverObjectName + "_Texture_Assets");
-                var posTex = RenderTextureToTexture2D.Convert(pRt);
-                var normTex = RenderTextureToTexture2D.Convert(nRt);
-                var tanTex = RenderTextureToTexture2D.Convert(tRt);
-                Graphics.CopyTexture(pRt, posTex);
-                Graphics.CopyTexture(nRt, normTex);
-                Graphics.CopyTexture(tRt, tanTex);
-                Material mat =  CreateAnimatedObjects.CopyMaterial(template);
-                mat.shader = playShader;
-                mat.SetTexture("_MainTex", skin.sharedMaterial.mainTexture);
-                mat.SetTexture("_PosTex", posTex);
-                mat.SetTexture("_NmlTex", normTex);
-                mat.SetFloat("_Length", clip.length);
-                if (clip.wrapMode == WrapMode.Loop)
-                {
-                    mat.SetFloat("_Loop", 1f);
-                    mat.EnableKeyword("ANIM_LOOP");
-                }
-                var go = new GameObject(saverObjectName + "." + saverAnimationName[index]);
-                go.AddComponent<MeshRenderer>().sharedMaterial = mat;
-                go.AddComponent<MeshFilter>().sharedMesh = skin.sharedMesh;
-                AssetDatabase.CreateAsset(posTex, Path.Combine(subFolderPath, pRt.name + ".asset"));
-                AssetDatabase.CreateAsset(normTex, Path.Combine(subFolderPath, nRt.name + ".asset"));
-                AssetDatabase.CreateAsset(tanTex, Path.Combine(subFolderPath, tRt.name + ".asset"));
-                var res = Path.Combine(subFolderPath, string.Format("{0}.{1}.animTex.asset", saverObjectName, saverAnimationName[index]));
-                Debug.Log($"{Path.Combine(subFolderPath, string.Format("{0}.{1}.animTex.asset", saverObjectName, saverAnimationName[index]))}");
-                AssetDatabase.CreateAsset(mat, res);
-                PrefabUtility.SaveAsPrefabAsset(go, Path.Combine(dFolderPath, go.name + ".prefab").Replace("\\", "/"));
-                AssetDatabase.SaveAssets();
-                AssetDatabase.Refresh();
-                index++;
-            }
-            catch (Exception e)
+            var pRt = new RenderTexture(texWidth, frames, 0, RenderTextureFormat.ARGBHalf);
+            pRt.name = string.Format("{0}.{1}.posTex", saverObjectName, saverAnimationName[index]);
+            var nRt = new RenderTexture(texWidth, frames, 0, RenderTextureFormat.ARGBHalf);
+            nRt.name = string.Format("{0}.{1}.normTex", saverObjectName, saverAnimationName[index]);
+            var tRt = new RenderTexture(texWidth, frames, 0, RenderTextureFormat.ARGBHalf);
+            tRt.name = string.Format("{0}.{1}.tanTex", saverObjectName, saverAnimationName[index]);
+            foreach (var rt in new[] { pRt, nRt, tRt })
             {
-                Debug.Log($"<color=#F07427>Error with saverAnimationName[{index}] " + e.Message +"</color>");
+                rt.enableRandomWrite = true;
+                rt.Create();
+                RenderTexture.active = rt;
+                GL.Clear(true, true, Color.clear);
             }
-#endif
+
+            for (var i = 0; i < frames; i++)
+            {
+                clip.SampleAnimation(gameObject, dt * i);
+                skin.BakeMesh(mesh);
+
+                var verexArry = mesh.vertices;
+                var normalArry = mesh.normals;
+                var tangentArray = mesh.tangents;
+                infoList.AddRange(Enumerable.Range(0, vCount)
+                    .Select(idx => new VertInfo()
+                    {
+                        position = verexArry[idx],
+                        normal = normalArry[idx],
+                        tangent = tangentArray[idx],
+                    })
+                );
+            }
+            var buffer = new ComputeBuffer(infoList.Count, System.Runtime.InteropServices.Marshal.SizeOf(typeof(VertInfo)));
+            buffer.SetData(infoList.ToArray());
+
+            var kernel = infoTexGen.FindKernel("CSMain");
+            uint x, y, z;
+            infoTexGen.GetKernelThreadGroupSizes(kernel, out x, out y, out z);
+
+            infoTexGen.SetInt("VertCount", vCount);
+            infoTexGen.SetBuffer(kernel, "Info", buffer);
+            infoTexGen.SetTexture(kernel, "OutPosition", pRt);
+            infoTexGen.SetTexture(kernel, "OutNormal", nRt);
+            infoTexGen.SetTexture(kernel, "OutTangent", tRt);
+            infoTexGen.Dispatch(kernel, vCount / (int)x + 1, frames / (int)y + 1, 1);
+
+            buffer.Release();
+
+#if UNITY_EDITOR
+            var folderPath = Path.Combine("Assets", saverFolderName);
+            if (!AssetDatabase.IsValidFolder(folderPath))
+                AssetDatabase.CreateFolder("Assets", saverFolderName);
+            var dFolderPath = Path.Combine(folderPath, saverObjectName + "_Animation"); 
+            if (!AssetDatabase.IsValidFolder(dFolderPath))
+                AssetDatabase.CreateFolder(folderPath, saverObjectName + "_Animation");
+            var subFolderPath =Path.Combine(dFolderPath, saverObjectName + "_Texture_Assets");
+            if (!AssetDatabase.IsValidFolder(subFolderPath))
+                AssetDatabase.CreateFolder(dFolderPath, saverObjectName + "_Texture_Assets");
+            var posTex = RenderTextureToTexture2D.Convert(pRt);
+            var normTex = RenderTextureToTexture2D.Convert(nRt);
+            var tanTex = RenderTextureToTexture2D.Convert(tRt);
+            
+            Graphics.CopyTexture(pRt, posTex);
+            Graphics.CopyTexture(nRt, normTex);
+            Graphics.CopyTexture(tRt, tanTex);
+            
+            Material mat =  CreateAnimatedObjects.CopyMaterial(template);
+            mat.shader = playShader;
+            mat.SetTexture("_MainTex", skin.sharedMaterial.mainTexture);
+            mat.SetTexture("_PosTex", posTex);
+            mat.SetTexture("_NmlTex", normTex);
+            mat.SetFloat("_Length", clip.length);
+            if (clip.wrapMode == WrapMode.Loop)
+            {
+                mat.SetFloat("_Loop", 1f);
+                mat.EnableKeyword("ANIM_LOOP");
+            }
+            var go = new GameObject(saverObjectName + "." + saverAnimationName[index]);
+            go.AddComponent<MeshRenderer>().sharedMaterial = mat;
+            go.AddComponent<MeshFilter>().sharedMesh = skin.sharedMesh;
+            AssetDatabase.CreateAsset(posTex, Path.Combine(subFolderPath, pRt.name + ".asset"));
+            AssetDatabase.CreateAsset(normTex, Path.Combine(subFolderPath, nRt.name + ".asset"));
+            AssetDatabase.CreateAsset(tanTex, Path.Combine(subFolderPath, tRt.name + ".asset"));
+            var res = Path.Combine(subFolderPath, string.Format("{0}.{1}.animTex.asset", saverObjectName, saverAnimationName[index]));
+            Debug.Log($"{Path.Combine(subFolderPath, string.Format("{0}.{1}.animTex.asset", saverObjectName, saverAnimationName[index]))}");
+            AssetDatabase.CreateAsset(mat, res);
+            PrefabUtility.SaveAsPrefabAsset(go, Path.Combine(dFolderPath, go.name + ".prefab").Replace("\\", "/"));
+            AssetDatabase.SaveAssets();
+            AssetDatabase.Refresh();
+            index++;
         }
+        catch (Exception e)
+        {
+            Debug.Log($"<color=#F07427>Error with saverAnimationName[{index}] " + e.Message +"</color>");
+        }
+#endif
+        return index;
     }
 }
---
+++ AnimationBaker/Assets/AnimationBakerTemplates/Scripts/GPUBake.cs
@@ -0,0 +1,150 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+namespace AnimationBakerTemplates.Scripts
+{
+    public struct VertInfo
+    {
+        public Vector3 position;
+        public Vector3 normal;
+        public Vector3 tangent;
+    }
+    public class GPUBake : MonoBehaviour
+    {
+        
+        public ComputeShader infoTexGen;
+        public Shader playShader;
+        public AnimationClip clip;
+        public Material template;
+
+        private readonly List<Texture2D> _positionTextures = new List<Texture2D>();
+        private readonly List<Texture2D> _normalTextures = new List<Texture2D>();
+        private readonly List<Texture2D> _tangensTextures = new List<Texture2D>();
+        
+        
+        private static readonly int s_MainTex = Shader.PropertyToID("_MainTex");
+        private static readonly int s_PosTex = Shader.PropertyToID("_PosTex");
+        private static readonly int s_NmlTex = Shader.PropertyToID("_NmlTex");
+        private static readonly int s_Length = Shader.PropertyToID("_Length");
+        private static readonly int s_Loop = Shader.PropertyToID("_Loop");
+
+        [ContextMenu("bake texture")]
+        void Bake()
+        {
+            var skins = GetComponentsInChildren<SkinnedMeshRenderer>();
+            foreach (var skin in skins)
+            {
+                BakeMeshRender(skin);
+            }
+        }
+        private void BakeMeshRender(SkinnedMeshRenderer skin)
+        {
+            var folderPath = Path.Combine("Assets", name);
+            if (!AssetDatabase.IsValidFolder(folderPath))
+                AssetDatabase.CreateFolder("Assets", name);
+            var localFolder = Path.Combine(folderPath, $"{name}_{skin.name}"); 
+            if (!AssetDatabase.IsValidFolder(localFolder))
+                AssetDatabase.CreateFolder(folderPath, $"{name}_{skin.name}");
+            
+            var vCount = skin.sharedMesh.vertexCount;
+            var texWidth = Mathf.NextPowerOfTwo(vCount);
+            var mesh = new Mesh();
+            var frames = Mathf.NextPowerOfTwo((int)(clip.length / 0.05f));
+            var dt = clip.length / frames;
+            var infoList = new List<VertInfo>();
+            var pRt = new RenderTexture(texWidth, frames, 0, RenderTextureFormat.ARGBHalf);
+            pRt.name = $"{name}_{skin.name}.posTex";
+            var nRt = new RenderTexture(texWidth, frames, 0, RenderTextureFormat.ARGBHalf);
+            nRt.name = $"{name}_{skin.name}.normTex";
+            var tRt = new RenderTexture(texWidth, frames, 0, RenderTextureFormat.ARGBHalf);
+            tRt.name = $"{name}_{skin.name}.tanTex";
+            foreach (var rt in new[] { pRt, nRt, tRt })
+            {
+                rt.enableRandomWrite = true;
+                rt.Create();
+                RenderTexture.active = rt;
+                GL.Clear(true, true, Color.clear);
+            }
+            for (var i = 0; i < frames; i++)
+            {
+                clip.SampleAnimation(gameObject, dt * i);
+                skin.BakeMesh(mesh);
+
+                var verexArry = mesh.vertices;
+                var normalArry = mesh.normals;
+                var tangentArray = mesh.tangents;
+                infoList.AddRange(Enumerable.Range(0, vCount)
+                    .Select(idx => new VertInfo()
+                    {
+                        position = verexArry[idx],
+                        normal = normalArry[idx],
+                        tangent = tangentArray[idx],
+                    })
+                );
+            }
+            
+            // compute by buffer
+            var buffer = new ComputeBuffer(infoList.Count, System.Runtime.InteropServices.Marshal.SizeOf(typeof(VertInfo)));
+            buffer.SetData(infoList.ToArray());
+
+            var kernel = infoTexGen.FindKernel("CSMain");
+            infoTexGen.GetKernelThreadGroupSizes(kernel, out var x, out var y, out _);
+            infoTexGen.SetInt("VertCount", vCount);
+            infoTexGen.SetBuffer(kernel, "Info", buffer);
+            infoTexGen.SetTexture(kernel, "OutPosition", pRt);
+            infoTexGen.SetTexture(kernel, "OutNormal", nRt);
+            infoTexGen.SetTexture(kernel, "OutTangent", tRt);
+            infoTexGen.Dispatch(kernel, vCount / (int)x + 1, frames / (int)y + 1, 1);
+
+            buffer.Release();
+            
+            // create asset
+            
+            var posTex = RenderTextureToTexture2D.Convert(pRt);
+            var normTex = RenderTextureToTexture2D.Convert(nRt);
+            var tanTex = RenderTextureToTexture2D.Convert(tRt);
+            
+            Material mat =  CreateAnimatedObjects.CopyMaterial(template);
+            mat.shader = playShader;
+            mat.SetTexture(s_MainTex, skin.sharedMaterial.mainTexture);
+            mat.SetFloat(s_Length, clip.length);
+            if (clip.wrapMode == WrapMode.Loop)
+            {
+                mat.SetFloat(s_Loop, 1f);
+                mat.EnableKeyword("ANIM_LOOP");
+            }
+
+            
+            mat = SaveAsset(posTex, localFolder,pRt.name, mat, 1);
+            mat = SaveAsset(normTex, localFolder,nRt.name, mat, 2);
+            mat = SaveAsset(tanTex, localFolder,tRt.name, mat, 3);
+            AssetDatabase.CreateAsset(mat, Path.Combine(localFolder, name) + ".animTex.asset");
+        }
+
+        private Material SaveAsset(Texture2D texture2D, string localFolder, string nameOfTexture, Material material, int mask = 1)
+        {
+            if (mask == 1)
+            {
+                _positionTextures.Add(texture2D);
+                material.SetTexture(s_PosTex, texture2D);
+            }
+            else if (mask == 2)
+            {
+                _normalTextures.Add(texture2D);
+                material.SetTexture(s_NmlTex, texture2D);
+            }
+            else if (mask == 3)
+            {
+                _tangensTextures.Add(texture2D);
+            }
+            
+            AssetDatabase.CreateAsset(texture2D, Path.Combine(localFolder, nameOfTexture) + ".asset");
+
+            return material;
+        }
+
+    }
+}
